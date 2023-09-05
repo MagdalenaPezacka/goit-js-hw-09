436c4a157e0972f4bc995cd5f0626262280759f1
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-09-05 23:14:45 +0200

    task1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,48 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColorStart() {
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    let color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, 1000);
+
+  console.log(timerId);
+}
+
+function changeColorStop() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', changeColorStart);
+stopBtn.addEventListener('click', changeColorStop);
+
+// startBtn.addEventListener('click', disableButton);
+
+// function disableButton() {
+//     startBtn.disabled = true;
+// }
+// function ableButton() {
+//     startBtn.disabled = false;
+// }
+
+
+//konspekt
+// startBtn.addEventListener("click", () => {
+//   timerId = setInterval(() => {
+//     console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
+
+// stopBtn.addEventListener("click", () => {
+//   clearInterval(timerId);
+//   console.log(`Interval with id ${timerId} has stopped!`);
+// });
