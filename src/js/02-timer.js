19249b549d5dd1b42b1cdc3aefa5f67147e06e17
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-09-06 23:51:09 +0200

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,3 +3,15 @@
 
 const value = document.querySelector('.value');
 const label = document.querySelector('.label');
+
+
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
