0059d08d193193fcd1292cc8ba485e6687b1714f
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-09-06 23:26:19 +0200

    css

---
+++ src/js/02-timer.js
@@ -0,0 +1,5 @@
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+
+const value = document.querySelector('.value');
+const label = document.querySelector('.label');
