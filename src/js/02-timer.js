1894c7a38b6a2858a57fad0aaf307ab4c4db1d53
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-09-07 23:38:55 +0200

    timer-init

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,38 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-const value = document.querySelector('.value');
-const label = document.querySelector('.label');
+const startBtn = document.querySelector('button[data-start]');
+const timer = document.querySelector('.timer');
+const picker = document.querySelector('#datetime-picker');
+startBtn.disabled = true;
 
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0].getTime() < Date.now()) {
+            window.alert('Please, choose a date in the future');
+        } else {
+            selectedDate = selectedDates[0].getTime();
+            startBtn.disabled = false;
+            // Report.success();
+        };
+    }
+}
 
+flatpickr(picker, options);
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
+// const dateBox = () => {
+//   const date = {
+//     enableTime: true,
+//     dateFormat: 'Y-m-d H:i',
+//   };
+//   console.log(date);
 // };
+// picker.addEventListener('input', throttle(dateBox, 500));
+// {
+//     enableTime: true,
+//     dateFormat: "Y-m-d H:i",
+// }
