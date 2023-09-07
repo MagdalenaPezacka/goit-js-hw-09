0d8fe6a98c1a0736e0b8d8a7132745894d45b7c8
MagdalenaPezacka <129729072+MagdalenaPezacka@users.noreply.github.com>
2023-09-07 21:39:31 +0000

    Deploying to gh-pages from @ MagdalenaPezacka/goit-js-hw-09@1894c7a38b6a2858a57fad0aaf307ab4c4db1d53 🚀

--- 02-timer.051c88ac.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".value"),document.querySelector(".label");
-//# sourceMappingURL=02-timer.051c88ac.js.map
--- 02-timer.a1635115.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".value"),document.querySelector(".label");
-//# sourceMappingURL=02-timer.a1635115.js.map
